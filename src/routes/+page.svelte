b620def41d9bb775c34a93206cf0ccbb2e8de610
Luca Valsecchi <luca.valsecchi@noku.io>
2023-09-01 20:15:29 +0100

    feat(login)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1 class="text-green-700">Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	import { goto } from "$app/navigation";
+</script>
+
+<div class="flex flex-col justify-center items-center">
+	<h1 class="text-green-700 text-lg mb-2">Welcome to QR Codes generator</h1>
+	<p class="mb-2">Login to see qr codes generator</p>
+	<button on:click={() => goto("/login")} class="btn btn-primary w-1/5"
+		>Login Page</button
+	>
+</div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import background from "$lib/images/background.png";
 	import title from "$lib/images/title.png";
+	import { onMount } from "svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	let timer = 10;
+	onMount(() => {
+		const interval = setInterval(() => {
+			timer--;
+			if (timer === 0) {
+				clearInterval(interval);
+			}
+		}, 1000);
+	});
+
+	$: {
+		if (timer === 0) timer = 50;
+	}
 </script>
 
 <div class="h-screen w-screen relative">
@@ -12,24 +27,25 @@
 		style="background-image: url({background});"
 	/>
 	<div
-		class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-2/3 flex flex-col justify-center items-start p-8"
-		style="width: 50%"
+		class="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-2/3 flex flex-col justify-center items-start p-8"
 	>
-		<img alt="title" src={title} class="mb-2" />
+		<img alt="title" src={title} class="mb-2 w-80" />
 		<div
 			class="w-full h-full relative flex flex-col justify-center items-start p-8"
 			style="border: 40px solid rgba(255, 255, 255, 0.2);"
 		>
-			<div class="w-1/3 font-bold text-white">
+			<div class="w-2/3 font-medium text-white mb-auto text-4xl mt-auto">
 				HURRY UP! SCAN THE QR-CODE AND CLAIM THE PRIZE BEFORE IT EXPIRES!
 			</div>
-			<div class="w-1/3 text-white">Lugano, 7-11 september 2023</div>
+			<div class="w-2/3 text-white text-xl font-medium">
+				Lugano, 7-11 september 2023
+			</div>
 		</div>
 		<div
-			class="absolute flex justify-center items-center"
-			style="top: -10%; left: 60%; height: 60%; width: 40%; background: #463E3E"
+			class="absolute flex justify-center items-center left-3/4 bottom-1/4 h-3/4 w-1/3 text-9xl font-extralight text-white"
+			style="background: #463E3E"
 		>
-			10
+			{timer}
 		</div>
 	</div>
 </div>
