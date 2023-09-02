d73a2776f0b0b8620dbeb1fd44fc131cf74e9135
Luca Valsecchi <luca.valsecchi@noku.io>
2023-09-02 09:08:22 +0100

    feat(auth): user and password

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -6,5 +6,5 @@
 	<slot />
 {:else}
 	<p class="text-error my-10">You must be logged in to view this page</p>
-	<a class="btn btn-primary" href="/login">Login Page</a>
+	<a class="btn btn-primary w-full" href="/login">Login Page</a>
 {/if}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { auth, user } from "$lib/firebase";
-	import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+	import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-	const signInWithGoogle = async () => {
-		const provider = new GoogleAuthProvider();
-		const credential = await signInWithPopup(auth, provider);
-		const idToken = await credential.user.getIdToken();
-		await fetch("/api/signin", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ idToken }),
-		});
+	let email = "";
+	let password = "";
 
-		goto("/home");
+	const signInWithPassword = async () => {
+		try {
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			const user = userCredential.user;
+			goto("/home");
+		} catch (error: any) {
+			const errorMessage = error.message;
+			alert(errorMessage);
+		}
 	};
 
 	const signOutSSR = async () => {
@@ -25,12 +28,34 @@
 </script>
 
 <h2>Login</h2>
-
+<div class="form-control">
+	<label class="label">
+		<span class="label-text">Your Email</span>
+	</label>
+	<label class="input-group">
+		<span>Email</span>
+		<input
+			bind:value={email}
+			type="text"
+			placeholder="info@site.com"
+			class="input input-bordered"
+		/>
+	</label>
+</div>
+<div class="form-control">
+	<label class="label">
+		<span class="label-text">Your Password</span>
+	</label>
+	<label class="input-group">
+		<span>Password</span>
+		<input type="password" class="input input-bordered" bind:value={password} />
+	</label>
+</div>
 {#if $user}
 	<h3>User logged</h3>
 	<button on:click={signOutSSR} class="btn btn-primary">Sign Out</button>
 {:else}
-	<button on:click={signInWithGoogle} class="btn btn-primary"
+	<button on:click={signInWithPassword} class="btn btn-primary"
 		>Login with Google</button
 	>
 {/if}
